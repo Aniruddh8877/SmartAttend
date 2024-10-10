c3f2d7b2536125d6b2c8b4a6d9947927b1945e90
Anant kumar <anant1042005@gmail.com>
2024-10-10 12:42:16 +0530

    refined the frontend

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div className="nav w-[100%] h-20 bg-white px-6 py-4 flex justify-between">
-        <div className="title">
-            <h2 className=' text-3xl font-bold text-pink-400 '>Smart Attend</h2>
-        </div>
-        <div className="navlinks h-full w-1/4 flex justify-end items-center "> 
-            <ul className='flex justify-between gap-6 text-1xl font-semibold'>
-                <a href=""><li>home</li></a>
-                <a href=""><li>contact</li></a>
-                <a href=""><li>about</li></a>
-                <a href=""><li>project</li></a>
-            </ul>
-        </div>
+    <div className="nav w-[100%] h-[10%] bg-[#ee6d52] px-6 py-4 flex justify-between font-extrabold">
+      <div className="title">
+        <h2 className='text-3xl'>Smart Attend</h2>
+      </div>
+      <div className="navlinks h-full w-1/4 flex justify-end items-center">
+        <ul className='flex justify-between gap-6 text-2xl'>
+            <Link to="/">Home</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/about">About</Link>
+            <Link to="/project">Project</Link>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- frontend/src/components/Teacherdash.jsx
+++ frontend/src/components/Teacherdash.jsx
@@ -3,12 +3,12 @@ import { useState } from "react";
 import "./App.css";
 
 export default function Teacherdash() {
-  return (
-	<div className="h-[100vh] w-[100%] ">
-		 <Navbar />
-		 <div className="main h-[90%] w-full bg-[Crimson] flex justify-center items-center">
-			<button className="py-2 px-9 text-2xl font-semibold rounded-xl bg-white" >this is Teacher dashboard</button>
-		 </div>
-	</div>
-  );
+	return (
+		<div className="h-[100vh] w-[100%] ">
+			<Navbar />
+			<div className="main h-[90%] w-full bg-[Crimson] flex justify-center items-center">
+				<button className="py-2 px-9 text-2xl font-semibold rounded-xl bg-white" >this is Teacher dashboard</button>
+			</div>
+		</div>
+	);
 }
